a7fa646f9ac3d559f6363dbb2cb2af9daa3d56de
tesfa373 <t76189857@gmail.com>
2025-07-27 22:19:59 +0300

    Implement shopping list manager with add, remove, view functionality

--- fns_and_dsa/shopping_list_manager.py
+++ fns_and_dsa/shopping_list_manager.py
@@ -1,11 +1,13 @@
 # shopping_list_manager.py
 
 def display_menu():
-    print("\nShopping List Manager")
+    print()
+    print("Shopping List Manager")
     print("1. Add Item")
     print("2. Remove Item")
     print("3. View List")
     print("4. Exit")
+
 
 def main():
     shopping_list = []
